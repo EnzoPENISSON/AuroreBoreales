b66935726bad65abd7eec37d92c8468543a106a3
Vincent DIRIBARNE <vdiribarnepro@gmail.com>
2026-04-14 09:17:46 +0200

    Python

--- rda/main.py
+++ rda/main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+def generate_files(base_path, start_year, end_year, start_month=1, end_month=12):
+    files = []
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    for year in range(start_year, end_year + 1):
+        for month in range(start_month if start_year == year else 1, end_month + 1 if end_year == year else 13):
+            files.append(f"{base_path}/{year}/{year}{month:02}_cleaned.csv")
+
+    return files
+
+kp_files = ["data/kp-compiled/kp_cleaned.csv"]
+x_files = generate_files("data/mag-kiruna-compiled", 2007, 2024, 1, 6)
+
+# Ace
+solar_wind_files = generate_files(
+    "data/solarwinds-ace-compiled",
+    2007,
+    2016,
+    1,
+    7
+)
+
+# Discover
+solar_wind_files += generate_files(
+    "data/solarwinds-dscovr-compiled",
+    2016,
+    2024,
+    7,
+    6
+)
+
+def run():
+    print(x_files)
+    print(solar_wind_files)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def smooth(inputs):
+    avg = 0
+
+    for x in inputs:
+        avg += x
+
+    avg /= len(inputs)
+
+    return avg
 
 
-# Press the green button in the gutter to run the script.
+def merge():
+    pass
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    run()
